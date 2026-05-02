256bad14facd577268e8b8675cd99e7efb91f2dd
houdajeon <houdaafebts678@gmail.com>
2026-05-02 15:37:05 +0100

    initial project and create folders

---
+++ src/main/java/com/letsplay/LetsPlayApiApplication.java
@@ -0,0 +1,14 @@
+package com.letsplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class LetsPlayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsPlayApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/letsplay/lets_play_api/LetsPlayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letsplay.lets_play_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsPlayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
